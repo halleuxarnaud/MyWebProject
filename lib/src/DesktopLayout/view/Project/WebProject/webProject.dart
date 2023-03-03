a29bddc0bea208281ea4b7c8c4bcd6efec406a1b
Arnaud Halleux <halleuxarnaud4@gmail.com>
2023-03-03 15:41:22 +0100

    Correction WebProject

--- lib/src/DesktopLayout/view/Project/WebProject/webProject.dart
+++ lib/src/DesktopLayout/view/Project/WebProject/webProject.dart
@@ -1,7 +1,12 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:mywebproject/components/color.dart';
+import 'package:mywebproject/src/DesktopLayout/widgets/builderprojectcard.dart';
 
 class WebProject extends StatelessWidget {
   const WebProject({super.key});
@@ -14,16 +19,51 @@ class WebProject extends StatelessWidget {
         height: maxSize.height,
         width: maxSize.width,
         color: MyWebProjectUI.kDefaultcolor,
-        child: const Center(
-          child: Text(
-            'WebProject',
-            style: TextStyle(
-              color: MyWebProjectUI.secondaryColor,
-              fontFamily: 'SpaceMono',
-              fontWeight: FontWeight.w400,
-              fontSize: 80,
+        child: Stack(
+          children: [
+            Container(
+              height: maxSize.height,
+              width: maxSize.width,
+              child: SvgPicture.asset(
+                'assets/image/reliefBack.svg',
+                width: maxSize.width,
+                color: Colors.white.withOpacity(0.04),
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
+            const Padding(
+              padding: EdgeInsets.all(80.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  BuilderProjectCard(
+                      'assets/image/mywebprojectlogo.png',
+                      'MyWebProject',
+                      "MyWebProject is a portfolio fully realized with Flutter and Dart ",
+                      true,
+                      'https://github.com/halleuxarnaud/MyWebProject'),
+                ],
+              ),
+            ),
+            Positioned(
+              height: 60,
+              width: 60,
+              top: 20,
+              left: 20,
+              child: RotatedBox(
+                quarterTurns: 90,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/image/exit.svg',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/src/DesktopLayout/widgets/builderprojectcard.dart
+++ lib/src/DesktopLayout/widgets/builderprojectcard.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:mywebproject/components/color.dart';
 import 'package:mywebproject/components/data.dart';
-import 'package:mywebproject/src/DesktopLayout/widgets/mouseTrackAnimation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class BuilderProjectCard extends StatefulWidget {
@@ -34,144 +33,173 @@ class _BuilderProjectCardState extends State<BuilderProjectCard> {
   @override
   Widget build(BuildContext context) {
     Size maxSize = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: MouseRegion(
-        onEnter: (event) {
-          setState(() {
-            isHovered = true;
-          });
-        },
-        onExit: (event) {
-          setState(() {
-            isHovered = false;
-          });
-        },
-        child: Stack(
-          children: [
-            widget.opensource
-                ? Positioned(
-                    top: 1,
-                    right: 1,
-                    child: Image.asset('assets/image/Opensource.png'))
-                : Container(),
-            AnimatedContainer(
-              width: 400,
-              duration: const Duration(milliseconds: 200),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 2,
-                  color: MyWebProjectUI.colorFontField,
-                ),
-                borderRadius: const BorderRadius.all(Radius.circular(15)),
-                boxShadow: isHovered
-                    ? [
-                        BoxShadow(
-                          color: Colors.white.withOpacity(0.1),
-                          spreadRadius: 15,
-                          blurRadius: 60,
-                          offset: const Offset(0, 3),
-                        ),
-                      ]
-                    : [],
+    return MouseRegion(
+      onEnter: (event) {
+        setState(() {
+          isHovered = true;
+        });
+      },
+      onExit: (event) {
+        setState(() {
+          isHovered = false;
+        });
+      },
+      child: Stack(
+        children: [
+          widget.opensource
+              ? Positioned(
+                  top: 1,
+                  right: 1,
+                  child: Image.asset('assets/image/Opensource.png'))
+              : Container(),
+          AnimatedContainer(
+            width: 400,
+            height: maxSize.height,
+            duration: const Duration(milliseconds: 200),
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 2,
+                color: MyWebProjectUI.colorFontField,
               ),
+              borderRadius: const BorderRadius.all(Radius.circular(15)),
+              boxShadow: isHovered
+                  ? [
+                      BoxShadow(
+                        color: Colors.white.withOpacity(0.1),
+                        spreadRadius: 15,
+                        blurRadius: 60,
+                        offset: const Offset(0, 3),
+                      ),
+                    ]
+                  : [],
+            ),
+            child: SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.only(
                     left: 20.0, right: 20, top: 25, bottom: 25),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: Container(
-                            height: 300,
-                            width: 300,
-                            child: Image.asset(
-                              widget.image,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        Container(
-                          color: MyWebProjectUI.containerTitleCardColor,
-                          child: Center(
-                            child: Text(
-                              widget.title,
-                              style: const TextStyle(
-                                  color: MyWebProjectUI.titleColorCard,
-                                  fontFamily: 'SpaceMono',
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        Text(
-                          widget.description,
-                          style: const TextStyle(
-                              color: MyWebProjectUI.colorFontField,
-                              fontFamily: 'SpaceMono',
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        )
-                      ],
-                    ),
-                    widget.opensource
-                        ? Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              OutlinedButton(
-                                  style: OutlinedButton.styleFrom(
-                                      foregroundColor: Colors.red,
-                                      backgroundColor:
-                                          MyWebProjectUI.kDefaultcolor,
-                                      side: const BorderSide(
-                                          width: 2, color: Colors.white),
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(8))),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(15.0),
-                                    child: Row(
-                                      children: [
-                                        const Text(
-                                          MyWebProjectData.openSourceText,
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.w400,
-                                              fontFamily: 'SpaceMono',
-                                              fontSize: 19),
-                                        ),
-                                        const SizedBox(
-                                          width: 10,
-                                        ),
-                                        Container(
-                                            height: 20,
-                                            width: 20,
-                                            child: SvgPicture.asset(
-                                              'assets/image/github-svgrepo-com.svg',
-                                              color: Colors.white,
-                                            ))
-                                      ],
-                                    ),
-                                  ),
-                                  onPressed: () {
-                                    launchUrl();
-                                  }),
-                              const SizedBox(
-                                width: 20,
+                child: Container(
+                  height: maxSize.height - 215,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: Container(
+                              height: 300,
+                              width: 300,
+                              child: Image.asset(
+                                widget.image,
                               ),
-                              OutlinedButton(
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Container(
+                            color: MyWebProjectUI.containerTitleCardColor,
+                            child: Center(
+                              child: Text(
+                                widget.title,
+                                style: const TextStyle(
+                                    color: MyWebProjectUI.titleColorCard,
+                                    fontFamily: 'SpaceMono',
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            widget.description,
+                            style: const TextStyle(
+                                color: MyWebProjectUI.colorFontField,
+                                fontFamily: 'SpaceMono',
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          )
+                        ],
+                      ),
+                      widget.opensource
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                OutlinedButton(
+                                    style: OutlinedButton.styleFrom(
+                                        foregroundColor: Colors.red,
+                                        backgroundColor:
+                                            MyWebProjectUI.kDefaultcolor,
+                                        side: const BorderSide(
+                                            width: 2, color: Colors.white),
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8))),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(15.0),
+                                      child: Row(
+                                        children: [
+                                          const Text(
+                                            MyWebProjectData.openSourceText,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w400,
+                                                fontFamily: 'SpaceMono',
+                                                fontSize: 19),
+                                          ),
+                                          const SizedBox(
+                                            width: 10,
+                                          ),
+                                          Container(
+                                              height: 20,
+                                              width: 20,
+                                              child: SvgPicture.asset(
+                                                'assets/image/github-svgrepo-com.svg',
+                                                color: Colors.white,
+                                              ))
+                                        ],
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      launchUrl();
+                                    }),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                OutlinedButton(
+                                    style: OutlinedButton.styleFrom(
+                                        foregroundColor: Colors.red,
+                                        backgroundColor:
+                                            MyWebProjectUI.kDefaultcolor,
+                                        side: const BorderSide(
+                                            width: 2, color: Colors.white),
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8))),
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(15.0),
+                                      child: Text(
+                                        MyWebProjectData.buttonText,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w400,
+                                            fontFamily: 'SpaceMono',
+                                            fontSize: 19),
+                                      ),
+                                    ),
+                                    onPressed: () {}),
+                              ],
+                            )
+                          : Container(
+                              width: maxSize.width,
+                              child: OutlinedButton(
                                   style: OutlinedButton.styleFrom(
                                       foregroundColor: Colors.red,
                                       backgroundColor:
@@ -193,39 +221,14 @@ class _BuilderProjectCardState extends State<BuilderProjectCard> {
                                     ),
                                   ),
                                   onPressed: () {}),
-                            ],
-                          )
-                        : Container(
-                            width: maxSize.width,
-                            child: OutlinedButton(
-                                style: OutlinedButton.styleFrom(
-                                    foregroundColor: Colors.red,
-                                    backgroundColor:
-                                        MyWebProjectUI.kDefaultcolor,
-                                    side: const BorderSide(
-                                        width: 2, color: Colors.white),
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(8))),
-                                child: const Padding(
-                                  padding: EdgeInsets.all(15.0),
-                                  child: Text(
-                                    MyWebProjectData.buttonText,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w400,
-                                        fontFamily: 'SpaceMono',
-                                        fontSize: 19),
-                                  ),
-                                ),
-                                onPressed: () {}),
-                          ),
-                  ],
+                            ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
